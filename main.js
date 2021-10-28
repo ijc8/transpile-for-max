50a2edf04f3e59d6f8af016ed6b35cabe6150b88
Ian Clester <ijc@ijc8.me>
2021-10-27 22:25:31 -0400

    Pretty good.

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 self.outlets = 3
 
 function play(pitch, velocity = 100, duration = 1000) {
+    // Send note data out to a `makenote` object.
     self.outlet(2, duration)
     self.outlet(1, velocity)
     self.outlet(0, pitch)
@@ -37,34 +38,47 @@ function euclid(hits, length, offset = 0) {
     return beats
 }
 
-const pitches = [36, 48, 60, 67, 69, 72, 77, 82]
+// Piece is 512 beats in 60 seconds.
+const pieceLength = 512
+const noteDur = 200
+const beatDur = (60 * 1000 - noteDur) / pieceLength
 
-async function buildup() {
+const pitches = [36, 48, 60, 67, 69, 72, 74, 75]
+
+async function playPiece() {
     self.running = true
 
-    const rhythms = {}
-    for (const [index, pitch] of pitches.entries()) {
-        rhythms[pitch] = euclid(index + 1, 8)
-        for (let i = 0; i < 16; i++) {
-            for (const [pitch, rhythm] of Object.entries(rhythms)) {
-                console.log(i, pitch)
-                if (rhythm[i % rhythm.length]) {
-                    console.log("Play", pitch)
-                    play(+pitch, 100, 200)
-                }
-            }
-            await sleep(117)
+    const rhythms = []
+    async function buildUp() {
+        // Section 1: Gradually layer on increasingly busy Euclidean rhythms.
+        for (const [index, pitch] of pitches.entries()) {
+            rhythms.push([pitch, euclid(index + 1, 8)])
+            await sleep(beatDur * 32)
+        }
+        rhythms[rhythms.length - 1][0] += 1
+    }
+
+    let silentProb = 0
+    async function tearDown() {
+        // Section 2: Gradually filter out more and more notes.
+        for (let i = 0; i < 256; i++) {
+            silentProb = i / 255
+            await sleep(beatDur)
         }
     }
 
-    // const beats = euclid(3, 8)
-    // for (let i = 0; self.running; i++) {
-    //     if (beats[i % beats.length]) {
-    //         play(60, 100, 200)
-    //         play(64, 100, 200)
-    //     }
-    //     await sleep(117)
-    // }
+    // Start section processes in the background.
+    buildUp().then(tearDown)
+
+    // Throughout both sections, play all the current layers.
+    for (let i = 0; self.running; i++) {
+        for (const [pitch, rhythm] of rhythms) {
+            if (rhythm[i % rhythm.length] && Math.random() > silentProb) {
+                play(pitch, 100, noteDur)
+            }
+        }
+        await sleep(beatDur)
+    }
 }
 
-self.buildup = buildup
+self.play = playPiece
